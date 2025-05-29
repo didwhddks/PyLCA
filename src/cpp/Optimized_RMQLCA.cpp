3daa51b580d381e4b0a4df80c7df5961a7ee11db
didwhddks <a29896000@gmail.com>
2025-05-29 17:46:47 +0800

    finish OptimizedRMQLCA

--- src/cpp/Optimized_RMQLCA.cpp
+++ src/cpp/Optimized_RMQLCA.cpp
@@ -6,74 +6,6 @@
 #include <cmath>
 #include <limits>
 
-template <typename T>
-class SegmentTree {
-public:
-  SegmentTree(int n = 0) {
-    init(n);
-  }
-
-  SegmentTree(const std::vector<T>& vec) {
-    init(vec);
-  }
-
-  void init(int n) {
-    this->n = n;
-    val.assign(4 * n, std::numeric_limits<T>::max());
-  }
-
-  void init(const std::vector<T>& vec) {
-    this->n = vec.size();
-    val.assign(4 * n, std::numeric_limits<T>::max());
-    for (int i = 0; i < n; ++i) {
-      set(i, vec[i]);
-    }
-  }
-
-  void pull(int i) {
-    val[i] = std::min(val[2 * i], val[2 * i + 1]);
-  }
-
-  void set(int idx, T _val, int i, int L, int R) {
-    if (L == R) {
-      val[i] = _val;
-      return;
-    }
-    int mid = (L + R) / 2;
-    if (idx <= mid) {
-      set(idx, _val, 2 * i, L, mid);
-    } else {
-      set(idx, _val, 2 * i + 1, mid + 1, R);
-    }
-    pull(i);
-  }
-
-  void set(int idx, T _val) {
-    set(idx, _val, 1, 0, n - 1);
-  }
-
-  T rangeQuery(int qL, int qR, int i, int L, int R) {
-    if (R < qL || qR < L) {
-      return std::numeric_limits<T>::max();
-    }
-    if (qL <= L && R <= qR) {
-      return val[i];
-    }
-    int mid = (L + R) / 2;
-    T a = rangeQuery(qL, qR, 2 * i, L, mid);
-    T b = rangeQuery(qL, qR, 2 * i + 1, mid + 1, R);
-    return std::min(a, b);
-  }
-
-  T rangeQuery(int qL, int qR) {
-    return rangeQuery(qL, qR, 1, 0, n - 1);
-  }
-
-private:
-  int n;
-  std::vector<T> val;
-};
-
 class OptimizedRMQLCA {
 public:
   OptimizedRMQLCA(int n = 0) {
@@ -81,7 +13,7 @@ public:
   }
 
   void init(int n) {
-    first_vis.assign(n + 1, 0);
+    first.assign(n + 1, 0);
     adj.assign(n + 1, std::vector<int>());
   }
 
@@ -91,7 +23,7 @@ public:
   }
 
   void dfs(int u = 1, int p = 0, int d = 0) {
-    first_vis[u] = euler_path.size();
+    first[u] = euler_path.size();
     euler_path.emplace_back(u);
     depth.emplace_back(d);
     for (int v : adj[u]) {
@@ -113,12 +45,13 @@ public:
     block_cnt = (M + block_size - 1) / block_size;
     block.assign(block_cnt, 0);
     mask.assign(block_cnt, 0);
-    for (int i = 0, j = 0, b = 0; i < M; ++i, ++j) {
-      if (j == block_size) j = 0, ++b;
+    for (int i = 0, j = 0, b = 0; i < M; i++, j++) {
+      if (j == block_size) j = 0, b++;
       if (j == 0 || depth[i] < depth[block[b]]) block[b] = i;
       if (j > 0 && (i >= M || depth[i] > depth[i - 1])) mask[b] += 1 << (j - 1);
     }
   }
+
   void build_sparse_table() {
     table_size = lg[block_cnt] + 1;
     st.assign(block_cnt, std::vector<int>(table_size));
@@ -134,6 +67,7 @@ public:
       }
     }
   }
+
   void precompute_in_block_RMQ() {
     ways = 1 << (block_size - 1);
     in_block_RMQ.assign(ways, std::vector<std::vector<int>>());
@@ -152,6 +86,7 @@ public:
       }
     }
   }
+
   void build() {
     dfs();
     precompute_log();
@@ -159,21 +94,23 @@ public:
     build_sparse_table();
     precompute_in_block_RMQ();
   }
-  int query(int u, int v) {
-    u = first_vis[u], v = first_vis[v];
+
+  int getLCA(int u, int v) {
+    u = first[u];
+    v = first[v];
     if (u > v) std::swap(u, v);
 
-    int u_block = u / block_size, v_block = v / block_size,
-        u_offset = u % block_size, v_offset = v % block_size;
+    int u_block = u / block_size, v_block = v / block_size;
+    int u_offset = u % block_size, v_offset = v % block_size;
 
     if (u_block == v_block)
       return euler_path[u_block * block_size +
                         in_block_RMQ[mask[u_block]][u_offset][v_offset]];
 
-    int mn1 = u_block * block_size +
+    int min1 = u_block * block_size +
               in_block_RMQ[mask[u_block]][u_offset][block_size - 1];
-    int mn2 = v_block * block_size + in_block_RMQ[mask[v_block]][0][v_offset];
-    int lca = depth[mn1] < depth[mn2] ? mn1 : mn2;
+    int min2 = v_block * block_size + in_block_RMQ[mask[v_block]][0][v_offset];
+    int lca = depth[min1] < depth[min2] ? min1 : min2;
 
     if (u_block + 1 < v_block) {
       int sz = lg[v_block - u_block - 1];
@@ -189,5 +126,5 @@ private:
   int M, block_size, block_cnt, table_size, ways;
   std::vector<std::vector<std::vector<int>>> in_block_RMQ;
   std::vector<std::vector<int>> adj, st;
-  std::vector<int> euler_path, depth, first_vis, lg, block, mask;
+  std::vector<int> euler_path, depth, first, lg, block, mask;
 };
